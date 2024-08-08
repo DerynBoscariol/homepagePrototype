4b9bb3f02a4d0d62afb63ef0761b761f5d97df44
Deryn Boscariol <derynb@rogers.com>
2024-08-08 13:14:09 -0400

    removed depreciated window.event

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Make the DIV element draggagle:
+// Make the DIV element draggable:
 var section = document.getElementById("dragdiv");
 var pull = document.getElementById("dragdivPull");
 dragElement(section);
@@ -6,10 +6,10 @@ dragElement(section);
 function dragElement(element) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
-pull.onmousedown = dragMouseDown;
+  pull.onmousedown = dragMouseDown;
 
   function dragMouseDown(e) {
-    e = e || window.event;
+    // No need for `window.event`, just use `e` directly
     e.preventDefault();
     // get the mouse cursor position at startup:
     //pos3 = e.clientX;
@@ -20,7 +20,6 @@ pull.onmousedown = dragMouseDown;
   }
 
   function elementDrag(e) {
-    e = e || window.event;
     e.preventDefault();
     // calculate the new cursor position:
     //pos1 = pos3 - e.clientX;
@@ -29,11 +28,11 @@ pull.onmousedown = dragMouseDown;
     pos4 = e.clientY;
     // set the element's new position:
     element.style.top = (element.offsetTop - pos2) + "px";
-    //elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    //element.style.left = (element.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
-    /* stop moving when mouse button is released:*/
+    /* stop moving when mouse button is released: */
     document.onmouseup = null;
     document.onmousemove = null;
   }
