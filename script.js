231b5ca752b3fd0ade6a7396547f693a0fb7e71c
Deryn Boscariol <derynb@rogers.com>
2024-08-15 01:46:17 -0400

    fixed all styling to match design(carousel needs to be fixed)

--- script.js
+++ script.js
@@ -12,15 +12,13 @@ console.log(pull);    // Should log the element or null if not found
   }
 
 function dragElement(element) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  var pos2 = 0,  pos4 = 0;
 
   pull.onmousedown = dragMouseDown;
 
   function dragMouseDown(e) {
-    // No need for `window.event`, just use `e` directly
     e.preventDefault();
     // get the mouse cursor position at startup:
-    //pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
@@ -30,13 +28,10 @@ function dragElement(element) {
   function elementDrag(e) {
     e.preventDefault();
     // calculate the new cursor position:
-    //pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
-    //pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
     element.style.top = (element.offsetTop - pos2) + "px";
-    //element.style.left = (element.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
